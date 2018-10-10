2175be166ac527b15a98aeb15a297d3dcd026388
Leonardo Micheloni <leonardo.micheloni@tokiota.com>
2018-10-10 17:48:31 +0200

    minimal identity server working

---
+++ ids/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace ids
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
